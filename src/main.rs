a44a4ec00a2fc8151638d75f518969a4e0db2972
James Bell <belljames@pm.me>
2022-07-21 22:35:54 -0700

    median puzzle

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let my_numbers = vec![2.0, 4.0, 5.0, 6.0];
+    println!("Median is {:?}", median(my_numbers));
+    let my_numbers = vec![2.0, 4.0, 6.0];
+    println!("Median is {:?}", median(my_numbers));
+}
+
+fn median(mut a: Vec<f32>) -> Option<f32> {
+    if a.is_empty() {
+        return None
+    }
+
+    a.sort_by(|x, y| x.partial_cmp(y).unwrap());
+
+    let n_elements = a.len();
+    let middle = n_elements / 2;
+
+    let median = match n_elements % 2 {
+        0 => a[middle] + a [middle - 1] / 2.0,
+        _ => a[middle],
+    };
+
+    Some(median)
 }
